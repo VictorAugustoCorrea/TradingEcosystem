1a5a7587045d4b11c3a8e3d57b86b61af8ed3f14
Victor Augusto <victorcorrea8@icloud.com>
2026-02-23 18:10:06 -0300

    Creating the market_update component

---
+++ src/exchange/market_data/market_update.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#ifndef TRADINGECOSYSTEM_MARKET_UPDATE_H
+#define TRADINGECOSYSTEM_MARKET_UPDATE_H
+
+#include <sstream>
+#include "low-latency-components/types.h"
+#include "low-latency-components/lock_free_queue.h"
+
+using namespace Common;
+
+namespace Exchange {
+    #pragma pack(push, 1)
+    enum class MEMarketUpdateType : uint8_t {
+        INVALID = 0,
+        ADD = 1,
+        MODIFY = 2,
+        CANCEL = 3,
+        TRADE = 4
+    };
+
+    inline std::string marketUpdateTypeToString( const MEMarketUpdateType type) {
+        switch ( type ) {
+            case MEMarketUpdateType::ADD:
+                return "ADD";
+            case MEMarketUpdateType::MODIFY:
+                return "MODIFY";
+            case MEMarketUpdateType::CANCEL:
+                return "CANCEL";
+            case MEMarketUpdateType::TRADE:
+                return "TRADE";
+            case MEMarketUpdateType::INVALID:
+                return "INVALID";
+        }
+        return "UNKNOWN";
+    }
+
+    struct MEMarketUpdate {
+        Qty qty_ = Qty_INVALID;
+        Side side_ = Side::INVALID;
+        Price price_ = Price_INVALID;
+        OrderId order_id_ = OrderId_INVALID;
+        Priority priority_ = Priority_INVALID;
+        TickerId ticker_id_ = TickerId_INVALID;
+        MEMarketUpdateType type_ = MEMarketUpdateType::INVALID;
+
+        [[nodiscard]]
+        auto toString() const {
+            std::stringstream ss;
+            ss << "MEMarketUpdate: "
+                << " [ "
+                << " Type: " << marketUpdateTypeToString( type_ )
+                << " Ticker: " << tickerIdToString( ticker_id_ )
+                << " Order_id: " << orderIdToString( order_id_ )
+                << " Side: " << sideToString ( side_ )
+                << " Qty: " << qtyToString( qty_ )
+                << " Price: " << priceToString( price_ )
+                << " Priority: " << priorityToString( priority_)
+                << " ] ";
+            return ss.str();
+        }
+    };
+    #pragma pack(pop)
+
+    typedef LFQueue<MEMarketUpdate> MEMarketUpdateLFQueue;
+}
+
+#endif //TRADINGECOSYSTEM_MARKET_UPDATE_H
