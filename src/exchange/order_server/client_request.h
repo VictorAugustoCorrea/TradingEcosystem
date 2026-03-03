9202fc4f3b562ff2e9e688e40a6d720f7871400b
Victor Augusto <victorcorrea8@icloud.com>
2026-03-03 19:01:55 -0300

    add new methods

--- src/exchange/order_server/client_request.h
+++ src/exchange/order_server/client_request.h
@@ -57,9 +57,26 @@ namespace Exchange {
         }
     };
 
+    struct OMClientRequest {
+        size_t seq_num_ = 0;
+        MEClientRequest me_client_request_;
+
+        [[nodiscard]]
+        auto toString() const {
+            std::stringstream ss;
+            ss  << "OMClientRequest: "
+                << " [ "
+                << " seq: " << seq_num_
+                << " " << me_client_request_.toString()
+                << " ] ";
+            return ss.str();
+        }
+    };
+
     #pragma pack(pop)
 
     typedef LFQueue<MEClientRequest> ClientRequestLFQueue;
+
 }
 
 #endif //TRADINGECOSYSTEM_CLIENT_REQUEST_H
