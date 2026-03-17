46e2c904de21e2f26190239ff99dcb696804fc92
Victor Augusto <victorcorrea8@icloud.com>
2026-03-17 17:14:18 -0300

    follow creating methods

--- src/trading/market_data/market_data_consumer.cpp
+++ src/trading/market_data/market_data_consumer.cpp
@@ -1,1 +1,42 @@
 #include "market_data_consumer.h"
+
+namespace Trading {
+    MarketDataConsumer::MarketDataConsumer(
+        const ClientId client_id,
+        Exchange::MEMarketUpdateLFQueue *market_updates,
+        const std::string &iface,
+        const std::string &snapshot_ip,
+        const int &snapshot_port,
+        const std::string &incremental_ip,
+        const int &incremental_port) :
+    incoming_md_updates_(market_updates),
+    run_( false ),
+    logger_("trading_market_data_consumer_" + std::to_string(client_id) + ".log"),
+    incremental_mcast_socket_(logger_),
+    snapshot_mcast_socket_(logger_),
+    iface_(iface),
+    snapshot_ip_(snapshot_ip),
+    snapshot_port_(snapshot_port) {
+
+        auto recv_callback = [this](auto socket) -> void {
+            recvCallback(socket);
+        };
+
+        incremental_mcast_socket_.recv_callback_ = recv_callback;
+        ASSERT(incremental_mcast_socket_.init(incremental_ip, iface, incremental_port, true) >= 0,
+            "Unable to create incremental mcast socket. Error: " + std::string(strerror(errno)));
+
+        ASSERT(incremental_mcast_socket_.join(incremental_ip),
+            "Join failed on: " + std::to_string(incremental_mcast_socket_.socket_fd_) + " error: " + std::string(strerror(errno)));
+    }
+
+    auto MarketDataConsumer::run() noexcept -> void {
+        logger_.log("%:% %() %. \n",
+            __FILE__, __LINE__, __func__,
+            getCurrentTimeStr(&time_str_));
+        while (run_) {
+            incremental_mcast_socket_.sendAndRecv();
+            snapshot_mcast_socket_.sendAndRecv();
+        }
+    }
+}
--- src/trading/market_data/market_data_consumer.h
+++ src/trading/market_data/market_data_consumer.h
@@ -14,18 +14,51 @@
 namespace Trading {
     class MarketDataConsumer {
     public:
+        MarketDataConsumer(
+        ClientId client_id,
+        Exchange::MEMarketUpdateLFQueue *market_updates,
+        const std::string &iface,
+        const std::string &snapshot_ip,
+        const int &snapshot_port,
+        const std::string &incremental_ip,
+        const int &incremental_port);
+
+        auto run() noexcept -> void;
+
+        auto start() -> void {
+            run_ = true;
+            ASSERT(Common::createAndStartThread(-1, "Trading/MarketDataConsumer", [this] { run(); }) != nullptr,
+                "Failed to start MarketData thread.");
+        }
+        auto stop() -> void {
+            run_ = false;
+        }
+
+        ~MarketDataConsumer() {
+            stop();
+            using namespace std::literals::chrono_literals;
+            std::this_thread::sleep_for(5s);
+        }
+
+        MarketDataConsumer() = delete;
+        MarketDataConsumer(const MarketDataConsumer & ) = delete;
+        MarketDataConsumer(const MarketDataConsumer &&) = delete;
+        MarketDataConsumer &operator=(const MarketDataConsumer & ) = delete;
+        MarketDataConsumer &operator=(const MarketDataConsumer &&) = delete;
+
     private:
         size_t next_exp_inc_seq_num_ = 1;
-        Exchange::MDPMarketUpdateLFQueue *incoming_md_updates_ = nullptr;
+        Exchange::MEMarketUpdateLFQueue *incoming_md_updates_ = nullptr;
         volatile bool run_ = false;
         std::string time_str_;
         Logger logger_;
-        McastSocket incremental_mcast_socket_;
+        McastSocket incremental_mcast_socket_, snapshot_mcast_socket_;
         bool in_recovery_ = false;
         const std::string iface_, snapshot_ip_;
         const int snapshot_port_;
         typedef std::map<size_t, Exchange::MEMarketUpdate> QueuedMarketUpdates;
         QueuedMarketUpdates snapshot_queued_msgs_, incremental_queued_msgs_;
+        auto recvCallback(McastSocket *socket) noexcept -> void;
     };
 }
 
