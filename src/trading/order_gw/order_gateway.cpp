5b4e64d15111285f28b21b429f956559d62a6210
Victor Augusto <victorcorrea8@icloud.com>
2026-03-24 18:36:56 -0300

    creating the client order gateway component

---
+++ src/trading/order_gw/order_gateway.cpp
@@ -0,0 +1,88 @@
+#include "order_gateway.h"
+
+namespace Trading {
+    OrderGateway::OrderGateway(
+        const ClientId client_id,
+        Exchange::ClientRequestLFQueue *client_requests,
+        Exchange::MEClientResponseLFQueue *client_responses,
+        const std::string &ip,
+        const std::string &iface,
+        const int port) :
+    client_id_(client_id),
+    ip_(ip),
+    iface_(iface),
+    port_(port),
+    outgoing_requests_(client_requests),
+    incoming_responses_(client_responses),
+    logger_("trading_gateway_" + std::to_string(client_id) + ".log"),
+    tcp_socket_(logger_) {
+        tcp_socket_.recv_callback_ = [this] (auto socket, auto rx_time) { recvCallback(socket, rx_time); };
+    }
+
+    auto OrderGateway::run() noexcept -> void {
+        logger_.log("%:% %() %.\n",
+            __FILE__, __LINE__, __func__,
+            getCurrentTimeStr(&time_str_));
+        while (run_) {
+            tcp_socket_.sendAndRecv();
+            for (auto client_request = outgoing_requests_ -> getNextToRead(); client_request; client_request = outgoing_requests_ -> getNextToRead()) {
+                logger_.log("%:% %() % Sending client id: %, seq: % %.\n",
+                    __FILE__, __LINE__, __func__,
+                    getCurrentTimeStr(&time_str_),
+                    client_id_,
+                    next_outgoing_seq_num_,
+                    client_request -> toString());
+                tcp_socket_.send(&next_outgoing_seq_num_, sizeof(next_outgoing_seq_num_));
+                tcp_socket_.send(client_request, sizeof(Exchange::MEClientRequest));
+                outgoing_requests_ -> updateReadIndex();
+                next_outgoing_seq_num_++;
+            }
+        }
+
+    }
+    auto OrderGateway::recvCallback(TCPSocket *socket, const Nanos rx_time) noexcept -> void {
+        logger_.log("%:% %() % Received Socket: %, len: % %.\n",
+            __FILE__, __LINE__, __func__,
+            getCurrentTimeStr(&time_str_),
+            socket -> socket_fd_,
+            socket -> next_rcv_valid_index_,
+            rx_time);
+
+        if (socket -> next_rcv_valid_index_ >= sizeof(Exchange::OMClientResponse)) {
+            size_t i = 0;
+            for ( ; i + sizeof(Exchange::OMClientResponse) <= socket -> next_rcv_valid_index_; i += sizeof(Exchange::OMClientResponse)) {
+                const auto response = reinterpret_cast<const Exchange::OMClientResponse *> (socket -> inbound_data_.data() + i);
+                logger_.log("%:% %() % Received %.\n",
+                    __FILE__, __LINE__, __func__,
+                    getCurrentTimeStr(&time_str_),
+                    response -> toString());
+
+                if (response -> me_client_response_.client_id_ != client_id_) {
+                    logger_.log("%:% %() % ERROR. Incorrect client id. Client id expected: %, received: %. \n",
+                        __FILE__, __LINE__, __func__,
+                        getCurrentTimeStr(&time_str_),
+                        client_id_,
+                        response -> me_client_response_.client_id_);
+                    continue;
+                }
+
+                if (response -> seq_num_ != next_exp_seq_num_) {
+                    logger_.log("%:% %() % ERROR. Incorrect sequence number. Client id: %, Seq num expected: %, Received: %. \n",
+                        __FILE__, __LINE__, __func__,
+                        getCurrentTimeStr(&time_str_),
+                        client_id_,
+                        next_exp_seq_num_,
+                        response -> seq_num_);
+                    continue;
+                }
+
+                ++next_exp_seq_num_;
+                const auto next_write = incoming_responses_ -> getNextToWriteTo();
+                *next_write = std::move(response -> me_client_response_);
+                incoming_responses_ -> updateWriteIndex();
+            }
+            memcpy(socket -> inbound_data_.data(), socket -> inbound_data_.data() + i, socket -> next_rcv_valid_index_ - i);
+            socket -> next_rcv_valid_index_ -= i;
+        }
+    }
+}
---
+++ src/trading/order_gw/order_gateway.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#ifndef TRADINGECOSYSTEM_ORDER_GATEWAY_H
+#define TRADINGECOSYSTEM_ORDER_GATEWAY_H
+
+#include <functional>
+#include "low-latency-components/macros.h"
+#include "low-latency-components/tcp_server.h"
+#include "low-latency-components/thread_utils.h"
+#include "exchange/order_server/client_request.h"
+#include "exchange/order_server/client_response.h"
+
+namespace Trading {
+    class OrderGateway {
+    public:
+        OrderGateway(
+            ClientId client_id,
+            Exchange::ClientRequestLFQueue *client_requests,
+            Exchange::MEClientResponseLFQueue *client_responses,
+            const std::string &ip,
+            const std::string &iface,
+            int port);
+
+        auto run() noexcept -> void;
+        auto recvCallback(TCPSocket *socket, Nanos rx_time) noexcept -> void;
+
+        auto start() -> void {
+            run_ = true;
+            ASSERT(tcp_socket_.connect(ip_, iface_, port_, false ) >= 0,
+                "Unable to connect to ip: " + ip_ + ", port: " + std::to_string(port_) + " on iface: " + iface_ + ". Error: " + std::to_string(errno));
+            ASSERT(createAndStartThread(-1, "Trading/OrderGateway", [this] { run(); }) != nullptr,
+                "Failed to start OrderGateway thread.");
+        }
+
+        auto stop() -> void {
+            run_ = false;
+        }
+
+        ~OrderGateway() {
+            stop();
+            using namespace std::literals::chrono_literals;
+            std::this_thread::sleep_for(5s);
+        }
+
+        OrderGateway() = delete;
+        OrderGateway(const OrderGateway & ) = delete;
+        OrderGateway(const OrderGateway &&) = delete;
+        OrderGateway & operator=(const OrderGateway & ) = delete;
+        OrderGateway & operator=(const OrderGateway &&) = delete;
+
+    private:
+        const ClientId client_id_;
+        std::string ip_;
+        const std::string iface_;
+        const int port_ = 0;
+        Exchange::ClientRequestLFQueue *outgoing_requests_ = nullptr;
+        Exchange::MEClientResponseLFQueue *incoming_responses_ = nullptr;
+        volatile bool run_ = false;
+        std::string time_str_;
+        Logger logger_;
+        size_t next_outgoing_seq_num_ = 1;
+        size_t next_exp_seq_num_ = 1;
+        TCPSocket tcp_socket_;
+    };
+}
+
+#endif //TRADINGECOSYSTEM_ORDER_GATEWAY_H
